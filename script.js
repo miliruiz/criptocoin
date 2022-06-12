cf73797959013eaf816ad2abfd3a979131ac264f
Milagros Abril Ruiz <abriferrin11@gmail.com>
2022-06-11 23:25:42 -0300

    terminado

--- script.js
+++ script.js
@@ -1,39 +1,67 @@
-const fetchDataBtn = document.querySelector('#fetchdata')
-const result = document.querySelector('#result')
 
-// gets data from API and sets the content of #result div
-const getData = function() {
-  fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=BTC&tsym=USD&limit=100')
-    .then(response => response.json())
-    .then(data => {
-      var resultado = data.Data.Data
-      var precios = resultado.map(x => x.open)
-      const ctx = document.getElementById('myChart').getContext('2d');
-     const myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-        labels: precios,
-        datasets: [{
-            label: 'precios',
-            data: precios,
-            borderWidth: 1,
-            tension: 0.5,
-        }]
-    },
-    options: {
-        scales: {
-            y: {
-                beginAtZero: true
+const getData = function (coin) {
+    fetch('https://min-api.cryptocompare.com/data/v2/histoday?fsym=' + coin + '&tsym=USD&limit=50')
+        .then(response => response.json())
+        .then(data => {
+            var resultado = data.Data.Data
+            var precios = resultado.map(x => x.open)
+            canvas = document.getElementById('myChart');
+            ctx = canvas.getContext('2d');
+            ctx.clearRect(0, 0, canvas.width, canvas.height);
+            let chartStatus = Chart.getChart("myChart");
+            if (chartStatus != undefined) {
+                chartStatus.destroy();
             }
-        }
-    }
-});
-    })
-    .catch(error => console.log(error))
+            const myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: precios,
+                    datasets: [{
+                        label: 'precios',
+                        data: precios,
+                        borderWidth: 3,
+                        tension: 0.6,
+                        borderColor: 'rgb(46, 178, 93)',
+                        pointStyle: 'line',
+                        borderDashOffSets: 0,
+                    }
+                ]
+                },
+                options: {
+
+                    plugins: {
+                        
+                    },
+
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                        
+                    }
+                }
+            }
+            
+            
+            
+            );
+        })
+        .catch(error => console.log(error))
 }
 
-// add event listener for #fetchdata button
-fetchDataBtn.addEventListener('click', getData)
+
+let items = document
+    .getElementById("criptos")
+    .querySelectorAll('li');
+
+let consulta = document.getElementById('consultas');
+
+for (var i = 0; i < items.length; i++) {
+    items[i].addEventListener("click", function (i) { getData(i.srcElement.id)});
+    items[i].addEventListener("click", consultas);
+}
 
 
-const item = document.querySelector('#fetchdata')
+function consultas(i) {
+    consulta.innerHTML = `<li>Has consultado por ${i.srcElement.id} </li>`;
+}
